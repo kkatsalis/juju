28c7801eaa40c60e8c73248f907d94687e15eca5
katsalis <kostas.katsalis@eurecom.fr>
2016-06-09 18:09:09 +0200

    env-v1

--- Python/general_function_snippets.py
+++ Python/general_function_snippets.py
@@ -1,4 +1,44 @@
-from jujuclient import Environment
-env = Environment.connect('manual')
+import json
 
+from types import *
+from jujuclient import Environment
+
+env = Environment.connect('local')
+
+print('********************  Facades\n')
+obj=env.facades
+assert type(obj) is DictType, "obj is not a dictionary" 
+print(obj)
+
+
+print('\n\n********************  Users\n')
+obj=env.users.list()
+assert type(obj) is DictType, "obj is not a dictionary" 
+print(obj)
+  
+
+print('\n\n********************  Charms\n')
+obj=env.charms.list()
+assert type(obj) is DictType, "obj is not a dictionary"
+assert type(obj) is DictType, "obj is not a dictionary" 
+print(obj)
+
+
+print('\n\n********************  Environment Config\n')
+obj=env.get_env_config()
+assert type(obj) is DictType, "obj is not a dictionary"
+assert type(obj) is DictType, "obj is not a dictionary" 
+print(obj)
+
+
+print('\n\n********************  Environment Constraints\n')
+obj=env.get_env_constraints()
+assert type(obj) is DictType, "obj is not a dictionary" 
+print(obj)
+
+print('\n\n********************  Environment Info\n')
+obj=env.info()
+assert type(obj) is DictType, "obj is not a dictionary" 
+print(obj)
+print json.dumps(obj, indent=2)  #same as before but beautiful
 
